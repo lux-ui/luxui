e060eebb507b421218c3428ea96b96244365b44b
Marek Purwin <purwinmarek16@gmail.com>
2022-12-18 11:24:59 +0100

    develop | Add rwd menu

--- src/app/common/shared.module.ts
+++ src/app/common/shared.module.ts
@@ -1,9 +1,10 @@
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { RwdMenuComponent } from './components/navbar/rwd-menu/rwd-menu.component';
 
 @NgModule({
-  declarations: [NavbarComponent],
+  declarations: [NavbarComponent, RwdMenuComponent],
   imports: [RouterModule],
   exports: [NavbarComponent],
 })
