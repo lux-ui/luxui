f3614164a78571d33bcf6c3871711176e513b05d
Marek Purwin <purwinmarek16@gmail.com>
2022-12-26 20:57:22 +0100

    Develop | Create content of colors, spacing and typography page

--- src/app/documentation/documentation.module.ts
+++ src/app/documentation/documentation.module.ts
@@ -1,14 +1,25 @@
+import { TypographyComponent } from './settings/pages/typography/typography.component';
+import { SpacingComponent } from './settings/pages/spacing/spacing.component';
+import { ColorsComponent } from './settings/pages/colors/colors.component';
+import { CommonModule } from '@angular/common';
 import { DocumentationPageComponent } from './documentation-page/documentation-page.component';
 import { DocumentationRoutingModule } from './documentation-routing.module';
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { SharedModule } from '../common/shared.module';
 import { PaginationComponent } from './ui/pages/pagination/pagination.component';
 import { ButtonsComponent } from './elements/pages/buttons/buttons.component';
 import { TooltipComponent } from './ui/pages/tooltip/tooltip.component';
 
 @NgModule({
-  declarations: [DocumentationPageComponent, PaginationComponent, ButtonsComponent, TooltipComponent],
-  imports: [BrowserModule, DocumentationRoutingModule, SharedModule],
+  declarations: [
+    DocumentationPageComponent,
+    PaginationComponent,
+    ButtonsComponent,
+    TooltipComponent,
+    ColorsComponent,
+    SpacingComponent,
+    TypographyComponent,
+  ],
+  imports: [DocumentationRoutingModule, SharedModule, CommonModule],
 })
 export class DocumentationModule {}
