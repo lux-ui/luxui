542a52d451eb4e17ad2d4a56751b6620f90b9bb1
Marek Purwin <purwinmarek16@gmail.com>
2022-12-26 19:59:22 +0100

    Develop | Add basic documentation routing and few components

--- src/app/documentation/documentation-routing.module.ts
+++ src/app/documentation/documentation-routing.module.ts
@@ -1,8 +1,27 @@
+import { ButtonsComponent } from './elements/pages/buttons/buttons.component';
+import { TooltipComponent } from './ui/pages/tooltip/tooltip.component';
+import { PaginationComponent } from './ui/pages/pagination/pagination.component';
+import { TypographyComponent } from './settings/pages/typography/typography.component';
+import { SpacingComponent } from './settings/pages/spacing/spacing.component';
+import { ColorsComponent } from './settings/pages/colors/colors.component';
 import { DocumentationPageComponent } from './documentation-page/documentation-page.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: '', component: DocumentationPageComponent }];
+const routes: Routes = [
+  {
+    path: '',
+    component: DocumentationPageComponent,
+    children: [
+      { path: 'settings/colors', component: ColorsComponent },
+      { path: 'settings/spacing', component: SpacingComponent },
+      { path: 'settings/typography', component: TypographyComponent },
+      { path: 'ui/pagination', component: PaginationComponent },
+      { path: 'ui/tooltip', component: TooltipComponent },
+      { path: 'elements/buttons', component: ButtonsComponent },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/documentation/documentation.module.ts
+++ src/app/documentation/documentation.module.ts
@@ -3,9 +3,12 @@ import { DocumentationRoutingModule } from './documentation-routing.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { SharedModule } from '../common/shared.module';
+import { PaginationComponent } from './ui/pages/pagination/pagination.component';
+import { ButtonsComponent } from './elements/pages/buttons/buttons.component';
+import { TooltipComponent } from './ui/pages/tooltip/tooltip.component';
 
 @NgModule({
-  declarations: [DocumentationPageComponent],
+  declarations: [DocumentationPageComponent, PaginationComponent, ButtonsComponent, TooltipComponent],
   imports: [BrowserModule, DocumentationRoutingModule, SharedModule],
 })
 export class DocumentationModule {}
